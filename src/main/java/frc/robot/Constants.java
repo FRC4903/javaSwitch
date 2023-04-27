9f7c79eccd66f5a202ac5034188d06584f533a4c
Champion2005 <58636533+Champion2005@users.noreply.github.com>
2023-04-26 23:31:59 -0400

    prototype code

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -1,11 +1,14 @@
 package frc.robot;
 
+import java.util.HashMap;
+
 import com.revrobotics.CANSparkMax.IdleMode;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
 import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj2.command.Command;
 import frc.lib.config.SwerveModuleConstants;
 
 public final class Constants {
@@ -16,8 +19,8 @@ public final class Constants {
     public static final boolean invertGyro = false; // Always ensure Gyro is CCW+ CW-
 
     /* Drivetrain Constants */
-    public static final double trackWidth = Units.inchesToMeters(26);
-    public static final double wheelBase = Units.inchesToMeters(31);
+    public static final double baseWidth = Units.inchesToMeters(26);
+    public static final double baseLength = Units.inchesToMeters(31);
     public static final double wheelDiameter = Units.inchesToMeters(4.0);
     public static final double wheelCircumference = wheelDiameter * Math.PI;
 
@@ -29,10 +32,10 @@ public final class Constants {
 
     public static final SwerveDriveKinematics swerveKinematics =
         new SwerveDriveKinematics(
-            new Translation2d(wheelBase / 2.0, trackWidth / 2.0),
-            new Translation2d(wheelBase / 2.0, -trackWidth / 2.0),
-            new Translation2d(-wheelBase / 2.0, trackWidth / 2.0),
-            new Translation2d(-wheelBase / 2.0, -trackWidth / 2.0));
+            new Translation2d(baseLength / 2.0, baseWidth / 2.0),
+            new Translation2d(baseLength / 2.0, -baseWidth / 2.0),
+            new Translation2d(-baseLength / 2.0, baseWidth / 2.0),
+            new Translation2d(-baseLength / 2.0, -baseWidth / 2.0));
 
     /* Swerve Voltage Compensation */
     public static final double voltageComp = 12.0;
@@ -42,15 +45,15 @@ public final class Constants {
     public static final int driveContinuousCurrentLimit = 80;
 
     /* Angle Motor PID Values */
-    public static final double angleKP = 0.01;
+    public static final double angleKP = 0.0125;
     public static final double angleKI = 0.0;
-    public static final double angleKD = 0.0;
+    public static final double angleKD = 0.00025;
     public static final double angleKFF = 0.0;
 
     /* Drive Motor PID Values */
-    public static final double driveKP = 0.1;
+    public static final double driveKP = 0.022;
     public static final double driveKI = 0.0;
-    public static final double driveKD = 0.0;
+    public static final double driveKD = 0.0015;
     public static final double driveKFF = 0.0;
 
     /* Drive Motor Characterization Values */
@@ -122,14 +125,16 @@ public final class Constants {
   }
 
   public static final class AutoConstants {
+    public static HashMap<String, Command> autoEventMap = new HashMap<>();
+    
     public static final double kMaxSpeedMetersPerSecond = Units.feetToMeters(14);
     public static final double kMaxAccelerationMetersPerSecondSquared = 3;
     public static final double kMaxAngularSpeedRadiansPerSecond = Math.PI;
     public static final double kMaxAngularSpeedRadiansPerSecondSquared = Math.PI;
 
-    public static final double kPXController = 1;
-    public static final double kPYController = 1;
-    public static final double kPThetaController = 1;
+    public static final double kPXController = 0.025;
+    public static final double kPYController = 0.025;
+    public static final double kPThetaController = 0.0075;
 
     // Constraint for the motion profilied robot angle controller
     public static final TrapezoidProfile.Constraints kThetaControllerConstraints =
--- src/main/java/frc/robot/autos/exampleAuto.java
+++ src/main/java/frc/robot/autos/exampleAuto.java
@@ -1,32 +1,43 @@
 package frc.robot.autos;
 
-import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.controller.ProfiledPIDController;
-import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.math.geometry.Rotation2d;
-import edu.wpi.first.math.geometry.Translation2d;
-import edu.wpi.first.math.trajectory.Trajectory;
-import edu.wpi.first.math.trajectory.TrajectoryConfig;
-import edu.wpi.first.math.trajectory.TrajectoryGenerator;
-import edu.wpi.first.wpilibj2.command.InstantCommand;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
-import edu.wpi.first.wpilibj2.command.SwerveControllerCommand;
+
+import com.pathplanner.lib.PathConstraints;
+import com.pathplanner.lib.PathPlannerTrajectory;
+import com.pathplanner.lib.PathPlanner;
+import com.pathplanner.lib.auto.PIDConstants;
+import com.pathplanner.lib.auto.SwerveAutoBuilder;
+
 import frc.robot.Constants;
 import frc.robot.subsystems.Swerve;
-import java.util.List;
+
+import java.util.HashMap;
 
 public class exampleAuto extends SequentialCommandGroup {
   public exampleAuto(Swerve s_Swerve) {
     // This will load the file "Example Path.path" and generate it with a max velocity of 4 m/s and a max acceleration of 3 m/s^2
-    PathPlannerTrajectory examplePath = PathPlanner.loadPath("Example Path", new PathConstrains(4, 3));
+    PathPlannerTrajectory examplePath = PathPlanner.loadPath("Taxi Test", new PathConstraints(4, 3));
 
-    // This trajectory can then be passed to a path follower such as a PPSwerveControllerCommand
-    // Or the path can be sampled at a given point in time for custom path following
+    Constants.AutoConstants.autoEventMap.put("Score", new PrintCommand("Scoreeee"));
+    Constants.AutoConstants.autoEventMap.put("Pickup", new PrintCommand("Pickuppp"));
 
-    // Sample the state of the path at 1.2 seconds
-    PathPlannerState exampleState = (PathPlannerState) examplePath.sample(1.2);
+    // Create the AutoBuilder. This only needs to be created once when robot code starts, not every time you want to create an auto command. A good place to put this is in RobotContainer along with your subsystems.
+    SwerveAutoBuilder autoBuilder = new SwerveAutoBuilder(
+        s_Swerve::getPose, // Pose2d supplier
+        s_Swerve::resetOdometry, // Pose2d consumer, used to reset odometry at the beginning of auto
+        Constants.Swerve.swerveKinematics, // SwerveDriveKinematics
+        new PIDConstants(Constants.AutoConstants.kPXController, 0, 0), // PID constants to correct for translation error (used to create the X and Y PID controllers)
+        new PIDConstants(Constants.AutoConstants.kPThetaController, 0, 0), // PID constants to correct for rotation error (used to create the rotation controller)
+        s_Swerve::setModuleStates, // Module states consumer used to output to the drive subsystem
+        Constants.AutoConstants.autoEventMap,
+        true, // Should the path be automatically mirrored depending on alliance color. Optional, defaults to true
+        s_Swerve // The drive subsystem. Used to properly set the requirements of path following commands
+    );
 
-    // Print the velocity at the sampled time
-    System.out.println(exampleState.velocityMetersPerSecond);
+    Command fullAuto = autoBuilder.fullAuto(examplePath);
+    addCommands(fullAuto);
   }
 }
